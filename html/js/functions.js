0a4f6eb9af85c65f744ce3241b4b357350cef16d
bridos <bridos1234@gmail.com>
2019-02-13 16:36:27 +0200

    article info as foil, condition and language are taken as parameters on the updating of a narticle

--- html/js/functions.js
+++ html/js/functions.js
@@ -9,22 +9,30 @@ function getCards(){
 		type: "GET",
 		url: functionsFilePath+"?userId=1",
 		success: function (json){
-			var str ="<table><tr><th>mySell</th><th>Trend</th><th>Url</th><th>Foil</th><th>Amount</th><th>Action</th><th></th></tr>";
+			//var str ="<table><tr><th>mySell</th><th>Trend</th><th>Details</th><th>Action</th><th></th><th>Url</th></tr>";
+			var str ="<table><tr><th>mySell</th><th>Trend</th><th>Foil</th><th>Condition</th><th>Language</th><th>Amount</th>";
+			str+= "<th>Suggestions</th><th></th><th></th><th>Name</th></tr>";
 	     	//var json = JSON.parse(data);
 	     	$.each(json , function(i , item){
 	     		str += "<tr id="+json[i].articleId+">";
 	     		//str += "<td>" + json[i].id + "</td>";
 	     		str += "<td class = 'price'>" + json[i].mySell + "</td>";
 	     		str += "<td>" + json[i].trend + "</td>";
-	     		str += "<td>" + json[i].url + "</td>";
+	     		//str += "<td>" + getDetails(json[i].foil , json[i].condition , json[i].language) + "</td>";
 	     		str += "<td>" + json[i].foil + "</td>";
+	     		str += "<td code = "+json[i].condid+">" + json[i].condition + "</td>";
+	     		str += "<td code = "+json[i].langid+">" + json[i].language + "</td>";
+	     		//str += "<td>" + json[i].foil + "</td>";
 	     		str += "<td>" + json[i].amount + "</td>";
 	     		str += "<td><select id ="+json[i].articleId+"sel>" + getSelectFromJson(json[i]) + "</select></td>";
 	     		// str += "<td><select id = '" + json[i].articleId + "'<option selected='true'>Ad DB ignore rule</option><option>" +
 	     		//  Math.ceil(json[i].mySell*10)/10 + "</option><option>" + Math.floor(json[i].mySell*10)/10 + "</option>" +
 	     		//  "<option>" + Math.ceil(json[i].trend*10)/10 + "</option><option>" + Math.floor(json[i].trend*10)/10 +
 	     		//   "</option></select>";
-	     		str += "<td><button onclick='updatePrice("+json[i].articleId+")'>Go</button></td>";
+	     		condvar = '"'+json[i].condition+'"';
+	     		str += "<td><button onclick='updatePrice("+json[i].articleId+","+condvar+","+json[i].langid+","+json[i].foil+")'>Update</button></td>";
+	     		str += "<td><a href="+json[i].url+" target='_blank'>Visit</td>";
+	     		str += "<td>"+json[i].cardname+"</td>";
 	     		str += "</tr>";
 	     	})
 	     	str += "</table>"
@@ -33,6 +41,11 @@ function getCards(){
      	},
 		dataType: "json"
 	})
+}
+function getDetails(foil , cond , lang){
+	var str = "";
+	str += "foil:"+foil+" condition:"+cond+" language:"+lang;
+	return str;
 }
 function getSelectFromJson(json){
 	ret = "";
@@ -57,12 +70,20 @@ function onlyUnique(value, index, self) {
 }
 
 
-function updatePrice(articleId){
+function updatePrice(articleId , condition , language, foil){
 	price = $('#' + articleId+"sel").find(":selected").text();
-	console.log("price: "+price);
+	//console.log("cond: "+condition +" lang: "+language);
+	//return;
+	//console.log("price: "+price);
+	if(foil == 1){
+		foil = true;
+	} else{
+		foil = false;
+	}
 	$.ajax({
 		type: "GET",
-		url: "http://localhost/xampp/personalProjects/price_trend_getter_jquery/new_functions/putRequests/updatePrice.php?articleId="+articleId+"&price="+price,
+		url: "http://localhost/xampp/personalProjects/price_trend_getter_jquery/new_functions/putRequests/updatePrice.php?"+
+		"articleId="+articleId+"&price="+price+"&condition="+condition+"&language="+language+"&foil="+foil,
 		success: function(json){
 			console.log("in succ");
 			console.log(json);
--- putRequests/updatePrice.php
+++ putRequests/updatePrice.php
@@ -5,31 +5,49 @@ require_once '../var.php';
 if(!isset($_GET["articleId"])){
 	die("No articleId given, dying..");
 }
-
-new priceUpdater($_GET["articleId"] , $_GET["price"]);
+if(!isset($_GET["price"])){
+	die("Can't update a price without a price, dying..");
+}
+$params = array();
+// if(isset($_GET["language"])){
+// 	$params[] = array("language" => )
+//}
+$params["language"] = isset($_GET["language"]) ? $_GET["language"] : 1; 
+$params["foil"] =  isset($_GET["foil"]) ? $_GET["foil"] : false; 
+$params["condition"] =  isset($_GET["condition"]) ? $_GET["condition"] : 6;
+$params["amount"] = isset($_GET["amount"]) ? $_GET["amount"] : 1;
+//print_r($params);
+//die();
+new priceUpdater($_GET["articleId"] , $_GET["price"] , $params);
 
 class priceUpdater{
 	private $articleId;
 	private $price;
 	private $url = "https://api.cardmarket.com/ws/v2.0/stock";
+	/*
 	private $xml = '<?xml version="1.0" encoding="UTF-8" ?> 
 		<request>
 			<article>
-				<idArticle>::articleId::</idArticle>
-				<idLanguage>1</idLanguage>
-				<comments>Edited through the API</comments>
-				<count>1</count>
-        		<price>::price::</price>
-				<condition>GD</condition>
+				<idArticle>::ARTICLEID::</idArticle>
+				<idLanguage>::LANGUAGE::</idLanguage>
+				<comments>Don\'t fail me now</comments>
+				<count>::AMOUNT::</count>
+        		<price>::PRICE::</price>
+        		<isFoil>::FOIL::</isFoil>
+				<condition>::CONDITION::</condition>
 			</article>
 		</request>';
-	function __construct($articleId , $price){
+		*/
+	private $xml = "";
+	function __construct($articleId , $price , $params){
 		$this->articleId = $articleId;
 		$this->price = $price;
+		$this->params = $params;
 		$this->prepareXml();
 		$handler = new mkmApiConnectionInfo();
 		$handler->getUserCreds();
    		$handler->prepareRequest($this->url , "PUT");
+   		//die($this->xml);
    		$handler->execute($this->xml);
    		$this->updateInDB();
    		//$this->sendReply();
@@ -45,10 +63,37 @@ class priceUpdater{
 		}
 	}
 	function prepareXml(){
-		$this->xml = str_replace(
-			array("::articleId::" , "::price::") ,
-			array($this->articleId , $this->price),
-			$this->xml
-		);
+		// $this->xml = str_replace(
+		// 	array("::ARTICLEID::" , "::PRICE::" , "::CONDITION::" , "::LANGUAGE::" , "::AMOUNT::" , "::FOIL::") ,
+		// 	array($this->articleId , $this->price , $this->params["condition"] , $this->params["language"] , $this->params["amount"] , $this->params["foil"]),
+		// 	$this->xml
+		// );
+		
+		$this->xml = '<?xml version="1.0" encoding="UTF-8" ?> 
+		<request>
+			<article>
+				<idArticle>'.$this->articleId.'</idArticle>
+				<price>'.$this->price.'</price>';
+
+		if(isset($this->params['condition'])){
+			$this->xml .= "<condition>".$this->params['condition']."</condition>";
+		}
+		if(isset($this->params['language'])){
+			$this->xml .= "<idLanguage>".$this->params['language']."</idLanguage>";
+		}
+		if(isset($this->params['amount'])){
+			$this->xml .= "<count>".$this->params['amount']."</count>";
+		}
+		if(isset($this->params['foil'])){
+			if($this->params['foil'] === true){
+				echo "in!<br>";
+				$this->xml .= "<isFoil>".$this->params['foil']."</isFoil>";
+			} else{
+				//echo "foil param false<br>";
+			}
+		}
+		$this->xml .= 
+			"</article>
+		</request>";
 	}
 }
